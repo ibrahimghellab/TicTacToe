0097a912a2889bbedea25b39dd895d3304dc07c0
ibrahimghellab <ghellabibrahim@gmail.com>
2024-11-06 23:59:54 +0100

    refont of the html with new ids and use of a 2D array in js

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-let matrice=[0,0,0,0,0,0,0,0,0];
-
 let joueur=0;
 
+let matrice=[[0,2,3],[4,5,6],[7,8,9]];
+
+let isWin=(i,j)=>{
+    
+}
 
 
-
-let clickButton=(indice)=>{
+let clickButton=(i,j)=>{
     if(joueur%2===0){
-        matrice[indice]=-1;
-        document.getElementById(`case${indice+1}`).style.backgroundImage=`url("./images/circle.jpg")`;
+        matrice[i][j]=-1;
+        document.getElementById(`case${i+1}${j+1}`).style.backgroundImage=`url("./images/circle.jpg")`;
         console.log(matrice);
         
     }else{
-        matrice[indice]=1;
-        document.getElementById(`case${indice+1}`).style.backgroundImage=`url("./images/cross.png")`;
+        matrice[i][j]=1;
+        document.getElementById(`case${i+1}${j+1}`).style.backgroundImage=`url("./images/cross.png")`;
         console.log(matrice);
     }
     joueur++;
-    if(isWin){
-        alert("Win");
-    }
+    
 }
     
-for(let i=1;i<10;i++){
-    document.getElementById(`case${i}`).addEventListener("click",()=>clickButton(i-1));
+for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++){
+    document.getElementById(`case${i+1}${j+1}`).addEventListener("click",()=>clickButton(i,j));
+    }
 }
 
 
